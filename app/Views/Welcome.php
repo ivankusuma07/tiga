619c5e14f7b784edbdd2ba31032955e9fd27b1a5
Ayub Narwidian Adiputra <ayubna32@gmail.com>
2015-07-14 14:51:19 +0700

    Create nice welcome landing page

---
+++ app/Views/Welcome.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<title>Welcome to Tiga !</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+	<style type="text/css">
+		
+		body {
+			background-color: #fafafa;
+			font-family: 'Open Sans', sans-serif;
+		}
+
+		section {
+			position: fixed;
+			top: 25%;
+			bottom: 25%;
+			left: 25%;
+			right: 25%;
+			text-align: center;
+			vertical-align: middle;
+		}
+
+		section h1 {
+			color: #232323;
+			font-family: 'Open Sans', sans-serif;
+			font-size: 5em;
+			font-weight: lighter;
+			margin-top: 40px;
+			margin-bottom: 25px;
+		}
+
+		section h1 label {
+			color: #f55030;
+			text-transform: uppercase;
+			font-weight: 400;
+		}
+
+		section h2 {
+			color: #888888;
+			font-family: 'Open Sans', sans-serif;
+			font-size: 1.8em;
+			font-weight: lighter;
+		}
+
+	</style>
+
+	<section>
+		<div class="brand">
+			<h1>
+				Welcome to <label>Tiga</label>
+			</h1>
+		</div>
+		<h2>Faux WordPress MVC Framework</h2>
+	</section>
+
+</body>
+
+</html>
