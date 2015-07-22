3c14cad396d46b7a8580debe7ca6d809309ac05c
todiadiyatmo <todiadiyatmo@tonjoo.com>
2015-07-22 22:41:14 +0700

    welcome.php

---
+++ app/Views/Welcome.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<title>Welcome to Tiga !</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+	<style type="text/css">
+		
+		body {
+			background-color: #fafafa;
+			font-family: 'Open Sans', sans-serif;
+		}
+
+		section {
+			position: fixed;
+			top: 35%;
+			bottom: 35%;
+			left: 25%;
+			right: 25%;
+			text-align: center;
+			vertical-align: middle;
+		}
+
+		section h1 {
+			color: #232323;
+			font-family: 'Open Sans', sans-serif;
+			font-size: 5em;
+			font-weight: lighter;
+			margin-top: 40px;
+			margin-bottom: 25px;
+		}
+
+		section h1 label a {
+			color: #f55030;
+			text-transform: uppercase;
+			font-weight: 400;
+			padding-left: 20px;
+			padding-right: 20px;
+			text-decoration: none;
+		}
+
+		section h1 label a:hover {
+			padding-left: 20px;
+			padding-right: 20px;
+			color: #fff;
+			text-transform: uppercase;
+			font-weight: 400;
+			background-color: #f55030;
+			border-radius: 2px;
+			-webkit-transition-duration: 1s;
+			transition-duration: 1s;
+			text-decoration: none;
+		}
+
+	</style>
+
+	<section>
+		<div class="brand">
+			<h1>
+				Welcome to 
+				<label>
+					<a href="http://tiga.io/" target="_blank">
+						Tiga
+					</a>
+				</label>
+			</h1>
+		</div>
+	</section>
+
+</body>
+
+</html>
